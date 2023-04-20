6d02d0773849954d66359bf58734eacd09b0212b
Mac <kaaso@space.dtu.dk>
2023-04-20 16:29:02 +0200

    added tests an

--- src/mae/__init__.py
+++ src/mae/__init__.py
@@ -4,6 +4,7 @@ from .analysis import *
 from .hyperparameter import *
 from .Visualisation import *
 from .CONSTANTS import *
+from . import CONSTANTS
 from ._mae_util import *
 from .CL_Logger import *
 
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+from .test_cae_model import *
+from .test_mae_model import *
+from .test_data import *
+from src import mae
