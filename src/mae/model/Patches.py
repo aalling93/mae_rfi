06c6c0ae6889388153201e98e17a3c92e89430b8
Mac <kaaso@space.dtu.dk>
2023-04-20 09:09:56 +0200

    comment patches

--- src/mae/model/Patches.py
+++ src/mae/model/Patches.py
@@ -1,11 +1,56 @@
+"""This script defines a custom Keras layer for extracting patches from images.
+
+The layer creates patches from the input images and returns the patches reshaped
+into (batch, num_patches, patch_area) for use in neural network training.
+
+Classes:
+    Patches: A custom Keras layer for extracting patches from images.
+
+Methods:
+    call: Extracts patches from the input images and returns the patches reshaped.
+    show_patched_image: A utility function that helps visualize one image and its
+        patches side by side.
+    reconstruct_from_patch: A utility function that takes patches from a single
+        image and reconstructs it back into the image.
+
+Example usage:
+    # Create a Patches layer with a patch size of (16, 16).
+    patches_layer = Patches(patch_size=(16, 16))
+
+    # Pass a batch of images through the layer to extract patches.
+    images = tf.random.normal((32, 256, 256, 3))
+    patches = patches_layer(images)
+
+    # Visualize a random image and its patches.
+    patches_layer.show_patched_image(images, patches)
+
+    # Reconstruct the original image from a single patch.
+    patch = patches[0, 0]
+    reconstructed = patches_layer.reconstruct_from_patch(patch)
+
+Author: Original OpenAI (modified by [Kristian Soerensen])
+Date: April 2023
+"""
+
+
 from ..CONSTANTS import *
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+
+
 class Patches(tf.keras.layers.Layer):
+    """A custom Keras layer for extracting patches from images."""
     def __init__(self, patch_size=PATCH_SIZE, **kwargs):
+        """Initializes the Patches layer.
+        
+        Args:
+            patch_size: A tuple representing the size of the patches to extract.
+            **kwargs: Additional keyword arguments passed to the base class.
+        """
         super().__init__(**kwargs)
         self.patch_size = patch_size
         # Assuming the image has three channels each patch would be
@@ -15,6 +60,17 @@ class Patches(tf.keras.layers.Layer):
         )
 
     def call(self, images):
+        """Extracts patches from the input images and returns the patches reshaped.
+        
+        Args:
+            images: A tensor representing a batch of input images.
+        
+        Returns:
+            A tensor of shape (batch_size, num_patches, patch_area), where
+            batch_size is the number of images in the batch, num_patches is
+            the number of patches extracted from each image, and patch_area
+            is the area of each patch.
+        """
         # Create patches from the input images
         patches = tf.image.extract_patches(
             images=images,
@@ -29,6 +85,15 @@ class Patches(tf.keras.layers.Layer):
         return patches
 
     def show_patched_image(self, images, patches):
+        """A utility function that helps visualize one image and its patches side by side.
+        
+        Args:
+            images: A tensor representing a batch of input images.
+            patches: A tensor representing the patches extracted from the input images.
+        
+        Returns:
+            The index of the image chosen to validate it outside the method.
+        """
         # This is a utility function which accepts a batch of images and its
         # corresponding patches and help visualize one image and its patches
         # side by side.
